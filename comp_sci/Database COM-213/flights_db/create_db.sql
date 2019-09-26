958595d92d00ec362f29a3139ed15064b442c3a2
Moritz Konarski <konarski_m@auca.kg>
2019-09-27 02:03:49 +0600

    finish flights db creation

--- comp_sci/Database COM-213/flights_db/create_db.sql
+++ comp_sci/Database COM-213/flights_db/create_db.sql
@@ -1,8 +1,13 @@
--- A database for an airport management situation
+CREATE DATABASE FlightsDb;
+GO
 
-/* SECTION PEOPLE */
--- table for persons: pilots, cabin crew, passengers
--- 
+USE FlightsDb;
+GO
+
+/* table as a basis for persons: pilots, cabin crew, passengers
+ * referenced by: Passengers, Bookings, ContactToPersons, Pilots
+ *  CabinCrew
+ */
 CREATE TABLE Persons (
   ID INT IDENTITY CONSTRAINT PK__Persons PRIMARY KEY CLUSTERED,
   Name NVARCHAR(255) NOT NULL,
@@ -13,7 +18,11 @@ CREATE TABLE Persons (
     UNIQUE (PassportNumber, Nationality)
 );
 
--- table for pilots
+/* table containing all pilots
+ * references: Persons
+ * referenced by: PilotsOnFlights, PlaneLicensesToPilots,
+ *  PilotsOfAirlines
+ */ 
 CREATE TABLE Pilots (
   ID INT IDENTITY CONSTRAINT PK__Pilots PRIMARY KEY CLUSTERED,
   PersonId INT NOT NULL,
@@ -21,7 +30,10 @@ CREATE TABLE Pilots (
     REFERENCES Persons(ID)
 );
 
--- table for cabin crew
+/* table containing all cabin crew members
+ * references: Persons
+ * referenced by: CabinCrewOnFlights, CabinCrewOfAirlines
+ */
 CREATE TABLE CabinCrew (
   ID INT IDENTITY CONSTRAINT PK__CabinCrew PRIMARY KEY CLUSTERED,
   PersonId INT NOT NULL
@@ -29,69 +41,91 @@ CREATE TABLE CabinCrew (
     REFERENCES Persons(ID)
 );
 
--- table for passengers
+/* table containing all passengers
+ * references: Persons
+ * referenced by: PassengersOnFlights, Luggage
+ */
 CREATE TABLE Passengers (
   ID INT IDENTITY CONSTRAINT PK__Passengers PRIMARY KEY CLUSTERED,
   PersonId INT NOT NULL,
-  NeedsAssistance BIT NOT NULL  -- for example elderly people, wheelchair-bound
+  -- for example elderly people, wheelchair-bound
+  NeedsAssistance BIT NOT NULL  
   CONSTRAINT FK__Passengers__Persons FOREIGN KEY (PersonId) 
     REFERENCES Persons(ID)
 );
 
-/* SECTION PLACES */
--- table of countries
+/* table for countries 
+ * referenced by: Contacts
+ */
 CREATE TABLE Countries (
   ID INT IDENTITY CONSTRAINT PK__Countries PRIMARY KEY CLUSTERED,
   Name VARCHAR(255) NOT NULL,
-  CountryAbbreviation VARCHAR(20) NOT NULL    -- for example GB for Great Britain
+  -- for example GB for Great Britain
+  CountryAbbreviation VARCHAR(20) NOT NULL
 );
 
--- table of contact information
-CREATE TABLE Contact (
-  ID INT IDENTITY CONSTRAINT PK__Contact PRIMARY KEY CLUSTERED,
+/* table for contact information for persons, airlines, airports
+ * references: Countries
+ * referenced by: ContactToAirports, ContactsToPersons, ContactsToAirlines
+ */
+CREATE TABLE Contacts (
+  ID INT IDENTITY CONSTRAINT PK__Contacts PRIMARY KEY CLUSTERED,
   PhoneNumber VARCHAR(30) NOT NULL,
   StreetAndNumber NVARCHAR(255) NOT NULL,
   City NVARCHAR(255) NOT NULL,
   PostCode INT NOT NULL,
   StateOrRegion NVARCHAR(255) NULL,
   Country INT NOT NULL,
-  CONSTRAINT FK__Contactn__Countries FOREIGN KEY (Country) 
+  CONSTRAINT FK__Contacts__Countries FOREIGN KEY (Country) 
     REFERENCES Countries(ID)
 );
 
--- table of airports
+/* table of airports
+ * referenced by: Terminals, ContactsToAirports
+ */
 CREATE TABLE Airports (
   ID INT IDENTITY CONSTRAINT PK__Airports PRIMARY KEY CLUSTERED,
   Name NVARCHAR(255) NOT NULL,
-  AirportAbbreviation VARCHAR(10) NOT NULL,        -- like FRU for Manas Airport
+  -- for example FRU for Manas Airport
+  AirportAbbreviation VARCHAR(10) NOT NULL
 );
 
--- table of terminals of airports
+/* table of terminals of airports
+ * references: Airports
+ * referenced by: Flights (2 times)
+ */
 CREATE TABLE Terminals (
   Name NVARCHAR(255) NOT NULL,
   AirportId INT NOT NULL,
-  CONSTRAINT PK__Terminals__Name__AirportId PRIMARY KEY (Name, AirportId),
+  CONSTRAINT PK__Terminals PRIMARY KEY (Name, AirportId),
   CONSTRAINT FK__Terminals__Airports FOREIGN KEY (AirportId) 
-    REFERENCES Airports(ID),
+    REFERENCES Airports(ID)
 );
 
-/* SECTION OBJECTS */
--- table for types of airlines
+/* table for the types of airlines, e.g. freight, military...
+ * referenced by: Airlines
+ */
 CREATE TABLE AirlineTypes (
-  ID INT IDENTITY CONSTRAINT PK__AirlineType PRIMARY KEY CLUSTERED,
+  ID INT IDENTITY CONSTRAINT PK__AirlineTypes PRIMARY KEY CLUSTERED,
   Name NVARCHAR(255) NOT NULL
 );
 
--- table for airlines
+/* table for airlines
+ * refereces: AirlineTypes
+ * referenced by: AirlinesToFlights, ContactsToAirlines, CabinCrewOfAirlines
+ *  PilotsOfAirlines, Planes
+ */
 CREATE TABLE Airlines (
   ID INT IDENTITY CONSTRAINT PK__Airlines PRIMARY KEY CLUSTERED,
   Name NVARCHAR(255) NOT NULL,
-  AirlineType INT NOT NULL          -- e.g. cargo, civillian, military...
+  AirlineType INT NOT NULL,
   CONSTRAINT FK__Airlines__AirlineTypes FOREIGN KEY (AirlineType) 
     REFERENCES AirlineTypes(ID)
 );
 
--- table for types/models of planes
+/* table containing types of planes, e.g. Boeing 747
+ * referenced by: Planes, PlaneLicenses
+ */
 CREATE TABLE PlaneTypes (
   Name NVARCHAR(255) CONSTRAINT PK__PlaneTypes PRIMARY KEY CLUSTERED,
   NumberOfRowsOfSeats INT NOT NULL,
@@ -100,19 +134,37 @@ CREATE TABLE PlaneTypes (
   MaximumBaggageCapacity INT NOT NULL,
 );
 
--- table for planes
+/* table containing the licenses required to fly the planes
+ * references: PlaneTypes
+ * referenced by: PlanesLicensedToPilots
+ */
+CREATE TABLE PlaneLicenses (
+  ID INT IDENTITY CONSTRAINT PK__PlaneLicenses PRIMARY KEY CLUSTERED,
+  PlaneTypeName NVARCHAR(255) NOT NULL,
+  CONSTRAINT FK__PlaneLicenses__PlaneTypes FOREIGN KEY (PlaneTypeName)
+    REFERENCES PlaneTypes(Name)
+);
+
+/* table containing individual planes
+ * references: PlaneTypes, Airlines
+ * referenced by: Flights
+ */
 CREATE TABLE Planes (
   ID INT IDENTITY CONSTRAINT PK__Planes PRIMARY KEY CLUSTERED,
   Callsign VARCHAR(255) NOT NULL CONSTRAINT UK__Planes__Callsign UNIQUE,
   AirlineId INT NOT NULL,
-  PlaneTypeName NVARCHAR(255) NOT NULL,         -- e.g. Airbus A-380, Boeing 747...
+  PlaneTypeName NVARCHAR(255) NOT NULL,
   CONSTRAINT FK__Planes__PlaneTypes FOREIGN KEY (PlaneTypeName) 
     REFERENCES PlaneTypes(Name),
   CONSTRAINT FK__Planes__Airlines FOREIGN KEY (AirlineId) 
     REFERENCES Airlines(ID)
 );
 
--- table for flights
+/* table containing each flight
+ * references: Terminals, Planes
+ * referenced by: Luggage, PassengersOnFlights, CabinCrewOnFlights,
+ *  AirlinesToFlights, PilotsOnFlights
+ */
 CREATE TABLE Flights (
   ID INT IDENTITY CONSTRAINT PK__Flights PRIMARY KEY CLUSTERED,
   PlaneId INT NOT NULL,
@@ -129,45 +181,36 @@ CREATE TABLE Flights (
     FOREIGN KEY (ArrivalTerminalName, ArrivalAirportId)
     REFERENCES Terminals(Name, AirportId),
   CONSTRAINT FK__Flights__Planes FOREIGN KEY (PlaneId) 
-    REFERENCES Planes(ID)
+    REFERENCES Planes(ID),
+  CONSTRAINT CK__Flights__Departure__Arrival__Times CHECK (DepartureTimeAndDate < ArrivalTimeAndDate)
 );
 
--- table for checked luggage on a flight
+/* table for checked luggage on specific flights
+ * references: Passengers, Flights
+ */
 CREATE TABLE Luggage (
   ID INT IDENTITY CONSTRAINT PK__Luggage PRIMARY KEY CLUSTERED,
   PassengerId INT NOT NULL,
   FlightId INT NOT NULL,
   Weight INT NOT NULL,
-  -- passengers on flights
   CONSTRAINT FK__Luggage__Passengers FOREIGN KEY (PassengerId) 
     REFERENCES Passengers(ID),
   CONSTRAINT FK__Luggage__Flights FOREIGN KEY (FlightId) 
     REFERENCES Flights(ID)
 );
 
--- table for airline status/memberships, e.g. Gold Card...
-CREATE TABLE RewardPrograms (
-  ID INT IDENTITY CONSTRAINT PK__RewardPrograms PRIMARY KEY CLUSTERED,
-  Name NVARCHAR(255) NOT NULL,
-  AirlineID INT NOT NULL
-);
-
--- table for types of payment for bookings, e.g. Credit, Cash...
+/* table that contains possible payment options for the flight booking
+ * referenced by: Bookings
+ */
 CREATE TABLE BookingPaymentTypes (
   ID INT IDENTITY CONSTRAINT PK__BookingPaymentTypes PRIMARY KEY CLUSTERED,
   Name NVARCHAR(255) NOT NULL
 );
 
--- table for types of airplane licenses that pilots need to fly them
-CREATE TABLE PlaneLicenses (
-  ID INT IDENTITY CONSTRAINT PK__PlaneLicenses PRIMARY KEY CLUSTERED,
-  PlaneTypeName NVARCHAR(255) NOT NULL,
-  CONSTRAINT FK__PlaneLicenses__PlaneTypes FOREIGN KEY (PlaneTypeName)
-    REFERENCES PlaneTypes(Name)
-);
-
-/* SECTION RELATIONSHIP TABLES */
--- table for bookings by persons
+/* table storing all bookings of flights
+ * references: BookingPaymentTypes, Persons
+ * referenced by: PassengersOnFlights
+ */
 CREATE TABLE Bookings (
   ID INT IDENTITY CONSTRAINT PK__Bookings PRIMARY KEY CLUSTERED,
   PersonId INT NOT NULL,
@@ -181,13 +224,17 @@ CREATE TABLE Bookings (
     REFERENCES BookingPaymentTypes(ID)
 );
 
--- table for passenger status types, e.g. business class, first class...
+/* table for the possible status of passengers, e.g. first class, economy...
+ * referenced by: PassengersOnFlights
+ */
 CREATE TABLE PassengerStatuses (
   ID INT IDENTITY CONSTRAINT PK__PassengerStatuses PRIMARY KEY CLUSTERED,
   Name NVARCHAR(255) NOT NULL,
 );
 
--- table for passengers on flights
+/* table connecting many passengers with many flights
+ * references: Bookings, PassengerStatuses, Passengers, Flights
+ */
 CREATE TABLE PassengersOnFlights (
   PassengerId INT NOT NULL,
   FlightId INT NOT NULL,
@@ -206,7 +253,9 @@ CREATE TABLE PassengersOnFlights (
     REFERENCES PassengerStatuses(ID)
 );
 
--- table for cabin crew on flights
+/* table connecting cabin crew to flights
+ * references: CabinCrew, Flights
+ */
 CREATE TABLE CabinCrewOnFlights (
   CabinCrewId INT NOT NULL,
   FlightId INT NOT NULL,
@@ -219,7 +268,9 @@ CREATE TABLE CabinCrewOnFlights (
     REFERENCES Flights(ID)
 );
 
--- table for pilots on flights
+/* table connection pilots to flights
+ * references: Pilots, Flights
+ */
 CREATE TABLE PilotsOnFlights (
   PilotId INT NOT NULL,
   FlightId INT NOT NULL,
@@ -232,7 +283,9 @@ CREATE TABLE PilotsOnFlights (
     REFERENCES Flights(ID)
 );
 
--- table for pilots to airlines
+/* connecting pilots to airlines
+ * references: Pilots, Airlines
+ */
 CREATE TABLE PilotsOfAirlines (
   PilotId INT NOT NULL,
   AirlineId INT NOT NULL,
@@ -247,7 +300,9 @@ CREATE TABLE PilotsOfAirlines (
     REFERENCES Airlines(ID)
 );
 
--- table for cabin crew to airlines
+/* connecting cabin crew to airlines
+ * references: CabinCrew, Airlines
+ */
 CREATE TABLE CabinCrewOfAirlines (
   CabinCrewId INT NOT NULL,
   AirlineId INT NOT NULL,
@@ -262,7 +317,9 @@ CREATE TABLE CabinCrewOfAirlines (
     REFERENCES Airlines(ID)
 );
 
--- table for plane licenses to pilots
+/* connecting plane licenses to pilots
+ * references: PlaneLicenses, Pilots
+ */
 CREATE TABLE PlaneLicensesToPilots (
   PlaneLicenseId INT NOT NULL,
   PilotId INT NOT NULL,
@@ -274,52 +331,48 @@ CREATE TABLE PlaneLicensesToPilots (
     REFERENCES Pilots(ID)
 );
 
--- table for airline loyalty programs to passengers
-CREATE TABLE AirlineLoyaltyProgramsToPassengers (
-  PassengerId INT NOT NULL,
-  AirlineLoyaltyProgramsId INT NOT NULL,
-  CONSTRAINT PK__AirlineLoyaltyProgramsToPassengers 
-    PRIMARY KEY (PassengerId, AirlineLoyaltyProgramsId),
-  CONSTRAINT FK__AirlineLoyaltyProgramsToPassengers__PassengerId FOREIGN KEY (PassengerId) 
-    REFERENCES Passengers(ID),
-  CONSTRAINT FK__AirlineLoyaltyProgramsToPassengers__AirlineLoyaltyPrograms 
-    FOREIGN KEY (AirlineLoyaltyProgramsId) REFERENCES AirlineLoyaltyPrograms(ID)
-);
-
--- table mapping contact information to persons
+/* connecting contact forms to persons
+ * referencing: Contacts, Persons
+ */
 CREATE TABLE ContactsToPersons (
   ContactId INT NOT NULL, 
   PersonId INT NOT NULL,
   CONSTRAINT PK__ContactsToPersons PRIMARY KEY (ContactId, PersonId),
   CONSTRAINT FK__ContactsToPersons__Contact FOREIGN KEY (ContactId)
-    REFERENCES Contact(ID),
+    REFERENCES Contacts(ID),
   CONSTRAINT FK__ContactsToPersons__Persons FOREIGN KEY (PersonId)
     REFERENCES Persons(ID)
 );
 
--- table mapping contact information to airports 
-CREATE TABLE ContactToAirports (
+/* connecting contacts to airports
+ * references: Airports, Contacts
+ */
+CREATE TABLE ContactsToAirports (
   ContactId INT NOT NULL, 
   AirportId INT NOT NULL,
-  CONSTRAINT PK__ContactToAirports PRIMARY KEY (ContactId, AirportId),
-  CONSTRAINT FK__ContactToAirports__Contact FOREIGN KEY (ContactId)
-    REFERENCES Contact(ID),
-  CONSTRAINT FK__ContactToAirports__Airports FOREIGN KEY (AirportId)
+  CONSTRAINT PK__ContactsToAirports PRIMARY KEY (ContactId, AirportId),
+  CONSTRAINT FK__ContactsToAirports__Contact FOREIGN KEY (ContactId)
+    REFERENCES Contacts(ID),
+  CONSTRAINT FK__ContactsToAirports__Airports FOREIGN KEY (AirportId)
     REFERENCES Airports(ID)
 );
 
--- table mapping contact information to airlines 
-CREATE TABLE ContactToAirlines (
+/* connecting contacts to Airlines
+ * references: Contacts, Airlines
+ */
+CREATE TABLE ContactsToAirlines (
   ContactId INT NOT NULL, 
   AirlineId INT NOT NULL,
-  CONSTRAINT PK__ContactToAirlines PRIMARY KEY (ContactId, AirlineId),
-  CONSTRAINT FK__ContactToAirlines__Contact FOREIGN KEY (ContactId)
-    REFERENCES Contact(ID),
-  CONSTRAINT FK__ContactToAirlines__Airlines FOREIGN KEY (AirlineId)
+  CONSTRAINT PK__ContactsToAirlines PRIMARY KEY (ContactId, AirlineId),
+  CONSTRAINT FK__ContactsToAirlines__Contact FOREIGN KEY (ContactId)
+    REFERENCES Contacts(ID),
+  CONSTRAINT FK__ContactsToAirlines__Airlines FOREIGN KEY (AirlineId)
     REFERENCES Airlines(ID)
 );
 
--- table mapping airlines to flights
+/* connecting Airlines to Flights
+ * references: Airlines, Flights
+ */
 CREATE TABLE AirlinesToFlights (
   ID INT IDENTITY CONSTRAINT PK__AirlinesToFlights PRIMARY KEY CLUSTERED,
   AirlineId INT NOT NULL,
@@ -329,12 +382,3 @@ CREATE TABLE AirlinesToFlights (
   CONSTRAINT FK__AirlinesToFlights__Flights FOREIGN KEY (FlightId)
     REFERENCES Flights(ID)
 );
-
-
-
-
-
-
-
-
-

b2225b3d375504cb9e212a227d973fa9b2978c89
Moritz Konarski <konarski_m@auca.kg>
2020-01-17 22:40:52 +0600

    CG: add init script, summary of notes

--- comp_sci/Computer Graphics COM-391/labs/init.sh
+++ comp_sci/Computer Graphics COM-391/labs/init.sh
@@ -8,39 +8,45 @@ touch "./$1/$1.cpp" "./$1/CMakeLists.txt" "./$1/conanfile.txt" "./$1/compile.sh"
 
 # Adding content to CMakeLists.txt
 
-# removed
-#add_definitions(\"std=c++11\")
-
-echo "cmake_minimum_required(VERSION \"2.8.0\")
+echo "
+cmake_minimum_required(VERSION \"2.8.0\")
 
 project(\"$1\")
 
+add_definitions(\"-std=c++11\")
+add_definitions(\"-G \"Visual Studio 16 2019\"\")
+add_definitions(\"-A \"x64\"\")
 
-include(build/conanbuildinfo.cmake)
+include(\${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()
 
 add_executable("$1" "$1".cpp)
-target_link_libraries("$1" \${CONAN_LIBS})" > "./$1/CMakeLists.txt"
+target_link_libraries("$1" \${CONAN_LIBS})
+" > "./$1/CMakeLists.txt"
 
 # Adding content to conanfile.txt
 
-echo "[requires]
+echo "
+[requires]
 sdl2/2.0.10@bincrafters/stable
 glew/2.1.0@bincrafters/stable
 
 [generators]
-cmake" > "./$1/conanfile.txt"
+cmake
+" > "./$1/conanfile.txt"
 
 # Adding content to cpp source file
 
-echo "#include <GL/glew.h>
-<#include <SDL.h>
+echo "
+#include <GL/glew.h>
+#include <SDL.h>
 #include <SDL_opengl.h> 
 
 int main(int argc, char **argv) {
     
     return 0;
-}" > "./$1/$1.cpp"
+}
+" > "./$1/$1.cpp"
 
 # Add content to compile file
 

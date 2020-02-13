4ecb30383d8b4435fba4a80dc4403fb7179170d8
Moritz Konarski <konarski_m@auca.kg>
2020-02-13 14:01:15 +0600

    RM: add lecture notes, start outline

--- math/Research_Methods_Applied_Math-MAT370/latex/midterm_project/compile.sh
+++ math/Research_Methods_Applied_Math-MAT370/latex/midterm_project/compile.sh
@@ -1,1 +1,3 @@
-pandoc proposal.md -o _proposal.pdf -V papersize=a4 -V linkcolor=blue -V geometry:margin=1in -V fontsize=12pt
+#!/bin/bash
+
+pandoc outline.md -o _outline.pdf -V papersize=a4 -V linkcolor=blue -V geometry:margin=1in -V fontsize=12pt -V numbersections=true #--toc 

e0e907bae9bc082920b70dd7f77a226e3137785d
Moritz Konarski <konarski_m@auca.kg>
2020-02-27 20:44:22 +0600

    CG: add new lab 05, modify init script

--- cs/Com_Graphics-COM391/labs/init_lab.sh
+++ cs/Com_Graphics-COM391/labs/init_lab.sh
@@ -26,7 +26,32 @@ pkg_search_module(GLM REQUIRED glm)
 
 include_directories(\${SDL2_INCLUDE_DIRS} \${GL_INCLUDE_DIRS} \${GLEW_INCLUDE_DIRS} \${GLM_INCLUDE_DIRS})
 
-add_executable("$1" "$1".cpp)
+add_executable(
+    "$1" 
+    include/scene/scene.hpp
+    include/objects/object.hpp
+    include/objects/camera.hpp
+    include/objects/mesh.hpp
+    include/geometries/vertex.hpp
+    include/geometries/geometry.hpp
+    include/geometries/es2_geometry.hpp
+    include/geometries/geometry_generators.hpp
+    include/materials/material.hpp
+    include/materials/constant_material.hpp
+    include/materials/es2_constant_material.hpp
+    include/textures/texture.hpp
+    include/textures/es2_texture.hpp
+    include/renderer/renderer.hpp
+    include/renderer/es2_renderer.hpp
+    include/renderer/shader.hpp
+    include/renderer/es2_shader.hpp
+    include/window/window.hpp
+    include/window/sdl_window.hpp
+    include/utilities/utilities.hpp
+    include/aur.hpp
+    "$1".cpp
+)
+
 target_link_libraries("$1" \${SDL2_LIBRARIES} \${GL_LIBRARIES} \${GLEW_LIBRARIES} \${GLM_LIBRARIES})
 " > "./$1/CMakeLists.txt"
 
